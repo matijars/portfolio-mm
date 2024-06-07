a8975455f715e72d77e30d7dea2ef3081e40e6cd
Matija Milosevic <matijamilosevic1988@gmail.com>
2024-06-07 13:07:31 +0200

    Firebase and socials
    - Added firebase hosting to project
    - Added links to social icons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { environment } from '../environments/environment';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
   ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD104bJL9JrC_1AqZrWoZH5tGTc1baoG-g',
+    authDomain: 'matijamilosevic-88.firebaseapp.com',
+    projectId: 'matijamilosevic-88',
+    storageBucket: 'matijamilosevic-88.appspot.com',
+    messagingSenderId: '438485146086',
+    appId: '1:438485146086:web:3d18811909237920b9de27',
+  },
+};
