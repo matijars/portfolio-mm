ad21c9791eddeec12f40586b680d9339e6543a17
Matija Milosevic <matijamilosevic1988@gmail.com>
2025-01-05 21:35:29 +0100

    Horisen update
    - Updated about section to include current job at Horisen
    - Added Horisen tab on Work section
    - Changed style of tech icons and separated them inside new ts file

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, ViewEncapsulation } from '@angular/core';
+import { TECH_ICONS } from '../../data/tech-icons';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
+  encapsulation: ViewEncapsulation.None, // Needed to svg styling inside scss
 })
 export class AboutComponent {
+  private sanitizer = inject(DomSanitizer);
 
+  // Sanitize the SVG strings and store them in an array
+  techIcons: { name: string; svg: SafeHtml }[] = TECH_ICONS.map((icon) => ({
+    name: icon.name,
+    svg: this.sanitizer.bypassSecurityTrustHtml(icon.svg),
+  }));
 }
